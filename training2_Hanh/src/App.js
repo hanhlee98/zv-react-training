4b16e121668515f6436df99d3ce98bd5ee7f4b0c
Hanhleuit <16520341@gm.uit.edu.vn>
2020-11-09 15:24:18 +0700

    task 3

---
+++ training2_Hanh/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Task3 from './Task3';
+
+function App() {
+  return (
+    <>
+      <Task3/>
+    </>
+  );
+}
+
+export default App;
---
+++ training2_Hanh/src/Task3/index.js
@@ -0,0 +1,67 @@
+import {useState, useCallback, useEffect} from 'react';
+import _ from 'lodash';
+
+const Tast3 = () => {
+    const [inputVal, setInputVal] = useState('')
+    const [isStopCountDown, setIsStopCountDown] = useState(false)
+    const [isValidInput, setIsValidInput] = useState(false)
+    const [count, setCount] = useState(undefined)
+
+    const handleInputChange = useCallback((e) => {
+        setInputVal(e.target.value)
+    }, [])
+
+    const handleStart = useCallback(() => {
+        console.log( (_.toNumber(inputVal)))
+        if (!inputVal) {
+            alert("Please enter a number")
+            return
+        }
+        if ( _.isNaN(_.toNumber(inputVal))) {
+            alert("Invalid input. Must be a number")
+            setInputVal('')
+            return
+        }
+        if (inputVal < 0) {
+            alert("Invalid input. Must be parseInta number")
+            setInputVal('')
+            return
+        }
+        setIsValidInput(true)
+        setCount(parseInt(inputVal))
+    }, [inputVal])
+
+    const handleStop = useCallback(() => {
+        setIsStopCountDown(true)
+    }, [])
+
+    useEffect(() => {
+        if (!isValidInput || isStopCountDown) {
+            return () => {
+            }
+        }
+        const countDown = setInterval(() => {
+            setCount(count => count - 1)
+        }, 1000)
+        return () => {
+            clearInterval(countDown)
+        }
+    }, [isValidInput, isStopCountDown])
+
+    useEffect(() => {
+        if (count <= 0) {
+            setIsStopCountDown(true)
+        }
+    }, [count])
+
+    return (
+        <>
+            <input value={inputVal} onChange={handleInputChange}/>
+            <button onClick={handleStart}>Start</button>
+            {isValidInput && <button onClick={handleStop}>Stop</button>}
+            {count >= 0 && count}
+        </>
+    )
+}
+
+export default Tast3;
