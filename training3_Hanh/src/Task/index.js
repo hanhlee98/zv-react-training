439c57f3a526459bbbbd07741f713081e1e568b8
Hanhleuit <16520341@gm.uit.edu.vn>
2020-11-11 18:08:36 +0700

    using Hook to handle keydown

--- training3_Hanh/src/Task/index.js
+++ training3_Hanh/src/Task/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useCallback, useEffect, useRef, useState} from 'react'
 
 const keyCode = {
     13: 'enter',
@@ -20,49 +20,54 @@ const keyCode = {
 }
 
 
-class Task extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputVal: '',
-            openModalMode: true,
-            logKeyArr: []
+const Task = () => {
+
+    const [inputVal, setVal] = useState('')
+    const [openModalMode, setModalMode] = useState(false)
+    const [logKeyArr, setLogKeyArr] = useState([])
+    const inputRef = useRef()
+
+    const handleOpenModal = useCallback(() => {
+        setModalMode(true)
+    }, [])
+
+    const handleChange = useCallback((e) => {
+        setVal(e.target.value)
+
+    }, [])
+
+    useEffect(() => {
+        if (!openModalMode) {
+            return () => {
+            }
         }
-    }
-
-    handleOpenModal = () => {
-        this.setState({
-            openModalMode: true
-        })
-    }
-    handleChange = (e) => {
-        this.setState({
-            inputVal: e.target.value
-        })
-
-    }
-    handleKeyDown = (e) => {
-        const {openModalMode, logKeyArr} = this.state
-        if (openModalMode) {
-            this.setState({
-                logKeyArr: [...logKeyArr, {
-                    key: Math.random(),
-                    val: keyCode[e.keyCode] ? keyCode[e.keyCode] : e.key
-                }]
-            })
+        if (!inputRef.current) {
+            return () => {
+            }
         }
-    }
+        const handleKeyDown = (e) => {
+            if (openModalMode) {
+                setLogKeyArr((logKeyArr) =>
+                    ([...logKeyArr, {
+                        key: Math.random(),
+                        val: keyCode[e.keyCode] ? keyCode[e.keyCode] : e.key
+                    }])
+                )
+            }
+        }
+        inputRef.current.addEventListener('keydown', handleKeyDown)
+        return () => {
+            inputRef.current.removeEventListener('keydown', handleKeyDown)
+        }
+    }, [openModalMode])
 
-    render() {
-        const {inputVal, logKeyArr} = this.state
-        return (
-            <div>
-                <input value={inputVal} onChange={this.handleChange} onKeyDown={this.handleKeyDown}/>
-                <button onClick={this.handleOpenModal}> Open Modal</button>
-                {logKeyArr.map((item) => (<div key={item.key}>{item.val}</div>))}
-            </div>
-        )
-    }
+    return (
+        <div>
+            <input value={inputVal} onChange={handleChange} ref={inputRef}/>
+            <button onClick={handleOpenModal}> Open Modal</button>
+            {logKeyArr.map((item) => (<div key={item.key}>{item.val}</div>))}
+        </div>
+    )
 }
 
 export default Task;
